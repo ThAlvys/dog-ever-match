f287f528960f0300842866526034d79ddf7af20d
ThAlvys <thiagoowcontato@gmail.com>
2024-08-14 09:50:48 -0300

    Wip

---
+++ app/(app)/inputScreen.tsx
@@ -0,0 +1,8 @@
+import { InputScreen } from "@/src/screen/inputScreen";
+
+
+export default function InputScreenApp() {
+    return(
+        <InputScreen/>
+    )
+}
---
+++ src/screen/inputScreen/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+
+
+export const InputScreen = () => {
+    return(
+        <View>
+            <Text>Hellow world</Text>
+        </View>
+    )
+}
